92ef4e188be0ab609ec9c2767e157e4b0ee6c0ec
flamelee2000 <flamelee2000@gmail.com>
2016-01-11 12:50:03 +0800

    change the Thread test

---
+++ JavaBasicTest/src/test/java/com/whoeveryou/FlameTest.java
@@ -0,0 +1,13 @@
+package com.whoeveryou;
+
+import org.junit.Test;
+
+public class FlameTest {
+
+	@Test
+	public void test() {
+		FlameNumber flameNumber=new FlameNumber(2,3);
+		System.out.println(flameNumber.addUp());
+	}
+
+}
---
+++ JavaBasicTest/src/test/java/com/whoeveryou/ThreadTest.java
@@ -0,0 +1,12 @@
+package com.whoeveryou;
+
+import org.junit.Test;
+
+public class ThreadTest {
+
+	@Test
+	public void test() {
+		System.out.println("Junit test");
+	}
+
+}
