e2246a0f0b3c3395b6c808ac0b6afe735d17d7e0
flamelee2000 <flamelee2000@gmail.com>
2016-01-16 23:29:54 +0800

    add log4j program.

---
+++ JavaBasicTest/src/main/java/com/whoeveryou/FlameLog4j.java
@@ -0,0 +1,17 @@
+package com.whoeveryou;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class FlameLog4j {
+    private static final Logger logger = LogManager.getLogger("Flame");
+    public static void main(String[] args){
+        logger.trace("Hello, Trace!");
+        logger.debug("Hello, Debug!");
+        logger.info("Hello, Info!");
+        logger.warn("Hello, Warn!");
+        logger.error("Hello, Error!");
+        logger.fatal("Hello, Fatal!");
+    }
+}
